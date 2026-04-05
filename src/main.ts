ae53af742074d25f4920727afba245849ec3a571
El Muchacho <privat@Bumayes-MacBook-Pro.local>
2026-04-05 19:51:59 +0200

    enable cors for netlify frontend

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['https://fastidious-cannoli-ae5bb9.netlify.app'],
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type'],
+  });
+
   await app.listen(Number(process.env.PORT) || 3000);
 }
 bootstrap();
