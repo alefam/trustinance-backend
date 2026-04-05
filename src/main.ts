6bdaa70a202ed997f765ab0a4562046975e0eae4
El Muchacho <privat@Bumayes-MacBook-Pro.local>
2026-04-05 20:00:17 +0200

    relax cors settings

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://fastidious-cannoli-ae5bb9.netlify.app'],
-    methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type'],
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Origin',
+    credentials: false,
+    optionsSuccessStatus: 204,
   });
 
   await app.listen(Number(process.env.PORT) || 3000);
